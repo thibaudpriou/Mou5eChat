9648c00da5f1a4fa4e467f3ec7ad392f9f415dfc
Thibaud Priou <thibaud.priou@gmail.com>
2018-03-24 20:15:57 +0100

    Add routes for mp3 files and update README

--- README.md
+++ README.md
@@ -3,7 +3,19 @@
 ### Installation
 * Install python 2.7
 * Install pip
-* then : pip install flask
+* Then install flask
+```
+pip install flask
+```
 
-### Running the server
-* python main.py
+### Running the server on the local machine
+```
+python app.py
+```
+
+## Create a tunnel to your server
+To tunnel the requests made on Recast.AI, download (ngrok)[https://ngrok.com/download], unzip it and start a HTTP tunnel with:
+```
+./ngrok start 8000
+```
+Then retrieve the forwarding address (ex : 'https://43ec6d93.ngrok.io' ) and use this address on Recast.AI.
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 import json
 
 app = Flask(__name__)
@@ -17,6 +17,22 @@ def index():
       'memory': { 'key': 'value' }
     }
   )
+
+@app.route('/bass/01', methods=['GET'])
+def get_bass_1():
+    filename = 'samples/bass/bass_01_140bpm.mp3'
+    return send_file(filename, mimetype='audio/mpeg')
+
+@app.route('/bass/02', methods=['GET'])
+def get_bass_2():
+    filename = 'samples/bass/bass_02_140bpm.mp3'
+    return send_file(filename, mimetype='audio/mpeg')
+
+@app.route('/bass/03', methods=['GET'])
+def get_bass_3():
+    filename = 'samples/bass/bass_03_140bpm.mp3'
+    return send_file(filename, mimetype='audio/mpeg')
+
 
 @app.route('/errors', methods=['POST'])
 def errors():
