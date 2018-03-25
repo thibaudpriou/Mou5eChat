d9072d450f97b5ff1231e8dda5e0c520f9fc5f47
Thibaud Priou <thibaud.priou@gmail.com>
2018-03-25 07:11:38 +0200

    Add good samples, clean routes and start /preview

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, send_file
-import json
+import json, audioop, wave
 
 app = Flask(__name__)
 port = 8000
@@ -7,75 +7,80 @@ port = 8000
 # to modify according to your ngrok address
 baseUrl = 'https://1e7fc766.ngrok.io'
 
-@app.route('/bass', methods=['POST'])
-def get_bass():
-  print(json.loads(request.get_data()))
+class Deserializer(object):
+  def __init__(self,j):
+    self.__dict__=j
+
+
+@app.route('/<instrument>/<int:sample_id>', methods=['GET'])
+def get_sample(instrument, sample_id):
+    filename = 'demo_samples/' + str(instrument) + '/' + str(instrument) + '_' + str(sample_id) + '.wav'
+    return send_file(filename, mimetype='audio/x-wav')
+
+@app.route('/<instrument>', methods=['POST'])
+def get_sample_choices(instrument):
+  # print(json.loads(request.get_data()))
   return jsonify(
     status=200,
     replies=[{
       'type': 'video',
-      'content': baseUrl+'/bass/01',
+      'content': baseUrl+'/'+ str(instrument) +'/1',
     }, {
       'type': 'video',
-      'content': baseUrl+'/bass/02',
+      'content': baseUrl+'/'+ str(instrument) +'/2',
     }, {
       'type': 'video',
-      'content': baseUrl+'/bass/03',
-    }],
-    conversation={
-      'memory': { 'key': 'value' }
-    }
+      'content': baseUrl+'/'+ str(instrument) +'/3',
+    }]
   )
 
-@app.route('/bass/01', methods=['GET'])
-def get_bass_1():
-    filename = 'samples/bass/bass_01_140bpm.mp3'
-    return send_file(filename, mimetype='audio/mpeg')
+@app.route('/preview', methods=['POST'])
+def get_preview():
+  data = request.get_data()
+  payload = Deserializer(json.loads(data))
+  conversation = Deserializer(payload.conversation)
+  memory = Deserializer(conversation.memory)
+  bass = None
+  drums = None
+  keys = None
+  guitar = None
 
-@app.route('/bass/02', methods=['GET'])
-def get_bass_2():
-    filename = 'samples/bass/bass_02_140bpm.mp3'
-    return send_file(filename, mimetype='audio/mpeg')
+  if hasattr(memory, 'bass'):
+    filename = 'demo_samples/bass/bass_' + str(memory.bass) + '.wav'
+    bass = wave.open(filename, 'r')
 
-@app.route('/bass/03', methods=['GET'])
-def get_bass_3():
-    filename = 'samples/bass/bass_03_140bpm.mp3'
-    return send_file(filename, mimetype='audio/mpeg')
+  if hasattr(memory, 'drums'):
+    filename = 'demo_samples/drums/drums_' + str(memory.drums) + '.wav'
+    drums = wave.open(filename, 'r')
 
-instrument = 'drums'
-@app.route('/'+instrument, methods=['POST'])
-def get_bass():
-  print(json.loads(request.get_data()))
-  return jsonify(
-    status=200,
-    replies=[{
-      'type': 'video',
-      'content': baseUrl+'/'instrument'/01',
-    }, {
-      'type': 'video',
-      'content': baseUrl+'/'+instrument+'/02',
-    }, {
-      'type': 'video',
-      'content': baseUrl+'/'+instrument+'/03',
-    }],
-    conversation={
-      'memory': { 'key': 'value' }
-    }
-  )
-@app.route('/'+instrument+'/01', methods=['GET'])
-def get_bass_1():
-    filename = 'samples/'+instrument+'/drum_01_94bpm.mp3'
-    return send_file(filename, mimetype='audio/mpeg')
+  if hasattr(memory, 'keys'):
+    filename = 'demo_samples/keys/keys_' + str(memory.keys) + '.wav'
+    keys = wave.open(filename, 'r')
 
-@app.route('/'+instrument+'/02', methods=['GET'])
-def get_drums_2():
-    filename = 'samples/'+instrument+'/drum_02_96bpm.mp3'
-    return send_file(filename, mimetype='audio/mpeg')
+  if hasattr(memory, 'guitar'):
+    filename = 'demo_samples/guitar/guitar_' + str(memory.guitar) + '.wav'
+    guitar = wave.open(filename, 'r')
 
-@app.route('/'+instrument+'/03', methods=['GET'])
-def get_drums_3():
-    filename = 'samples/'+instrument+'/drum_03_97bpm.mp3'
-    return send_file(filename, mimetype='audio/mpeg')
+  preview = None
+  for instrument in [bass, drums, keys, guitar]:
+    if (not instrument is None):
+      if preview is None:
+        preview = instrument
+      else:
+        # TODO merge audio with audioop
+        # TODO WARNING : some samples are 4sec long, others 2sec long, duplicate the small ones before mixing
+        # preview =
+        foo = 'foo' #TODO remove this line, I wanted to test something, do not care
+
+
+  # TODO write preview in a wav file ('wave.writeframesraw(preview)', I guess)
+  # TODO save this file in a dedicated folder
+  # TODO return an endpoint for Recast.AI
+  # TODO do the same for the '/mix' endpoint, but with 4 times the samples (you could even make a 'drop' after 4 periods, or 8, 12, ...)
+
+  return jsonify({
+
+  })
 
 @app.route('/errors', methods=['POST'])
 def errors():
